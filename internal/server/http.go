0cebbd7cd2ed182dca4703081806a2a2c0c374d7
Jack M <exegete@mac.com>
2023-07-09 12:36:22 -0700

    ch1p32 - Create HTTP server.

---
+++ internal/server/http.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func NewHTTPServer(addr string) *http.Server {
+	httpsrv := newHTTPServer()
+	r := mux.NewRouter()
+	r.HandleFunc("/", httpsrv.handleProduce).Methods("POST")
+	r.HandleFunc("/", httpsrv.handleConsume).Methods("GET")
+	return &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+}
